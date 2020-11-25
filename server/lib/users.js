7b0089d15bb65463f55897a174e11bbf37a8f73a
Juan Castillo <juanpablodlcc@gmail.com>
2020-11-24 21:11:24 -0600

    [FIX] config socket

--- server/lib/users.js
+++ server/lib/users.js
@@ -4,17 +4,24 @@ const haiku = require('./haiku');
 const users = {};
 
 // Random ID until the ID is not in use
-async function randomID() {
-  let id = haiku();
-  while (id in users) {
-    await Promise.delay(5);
-    id = haiku();
-  }
-  return id;
-}
+// async function randomID() {
+//   let id = haiku();
+//   while (id in users) {
+//     await Promise.delay(5);
+//     id = haiku();
+//   }
+//   return id;
+// }
 
 exports.create = async (socket) => {
-  const id = await randomID();
+  const params = new URLSearchParams(window.location.search)
+  const _params = {}
+  for (const param of params) {
+    const [key, value ]= param
+    _params[key] = value
+  }
+  const id = _params.uid
+  console.log("new socket at", id)
   users[id] = socket;
   return id;
 };
